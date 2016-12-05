ebab71b231e58771e266c71f38344f2777bdb444
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-12-05 23:12:14 +0100

    Move to TS 2.1

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
-var identity = require('folke-identity');
-var folke_core_1 = require('folke-core');
-var defaultComponents = require('folke-default-components');
-var defaultMenu = require('folke-default-menu');
-var localization = require('folke-ko-localization');
+var identity = require("folke-identity");
+var folke_core_1 = require("folke-core");
+var defaultComponents = require("folke-default-components");
+var defaultMenu = require("folke-default-menu");
+var localization = require("folke-ko-localization");
 function register(services) {
     localization.register({ identity: identity.fr });
     defaultMenu.register();
